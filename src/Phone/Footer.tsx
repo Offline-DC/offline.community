dfaaf178297384d6716ecb7f5a62288f4bfb3b6e
Jack Nugent <jacknugent@MacBookPro.hsd1.md.comcast.net>
2025-11-16 19:56:41 -0500

    Add footer

--- src/Phone/Footer.tsx
+++ src/Phone/Footer.tsx
@@ -1,17 +1,21 @@
 function Footer() {
-
-    const footerStyle = {
-        textAlign: "center" as const,
-        paddingTop: "1.1em" as const,
-        color: "white" as const,
-        fontSize: "0.8em" as const
-    }
-    const currentYear = new Date().getFullYear();
-    return (
-        <div style={footerStyle}>
-            © {currentYear} Offline Inc.
-        </div>
-    );
+  const footerStyle = {
+    textAlign: "center" as const,
+    paddingTop: "1.1em" as const,
+    color: "white" as const,
+    fontSize: "0.8em" as const,
+  };
+  return (
+    <div style={footerStyle}>
+      powered by{" "}
+      <a
+        style={{ color: "white", textDecoration: "underline" }}
+        href="https://dumb.co"
+      >
+        dumb.co
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
